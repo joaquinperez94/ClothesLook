34d504bef6a83bdff1b91743030717385c1bd597
joaquinperez94 <32302020+joaquinperez94@users.noreply.github.com>
2019-01-03 19:39:49 +0100

    Modelo y parte del esqueleto

--- ClothesLook/urls.py
+++ ClothesLook/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from clothesLookApp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.inicio),
+    path('prendas/', views.inicio),
+    path('looks/', views.inicio),
+    path('login/', views.inicio),
 ]
--- clothesLookApp/views.py
+++ clothesLookApp/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+#PAGINA DE INICIO
+def inicio(request):
+    return render(request,'inicio.html')
+
+#PAGINA DE PRENDAS
+def inicio(request):
+    return render(request,'prendas.html')
+
+#PAGINA DE LOOKS
+def inicio(request):
+    return render(request,'looks.html')
+
+#PAGINA DE LOGIN
+def inicio(request):
+    return render(request,'login.html')
